5dcde01c13e63e800d6f9960b7db0a637eebbc91
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-04-08 02:14:20 +0000

    Snapshot of branch develop taken on 2019-04-07T22:14:19.419-04:00 from "https://github.com/UnofficialJuliaMirror/RoMEPlotting.jl-238d586b-a4bf-555c-9891-eda6fc5e55a2"

---
+++ src/RoMEPlotting.jl
@@ -0,0 +1,71 @@
+module RoMEPlotting
+
+using Graphs
+using KernelDensityEstimate, KernelDensityEstimatePlotting
+using IncrementalInference, RoME
+using Gadfly
+using Colors
+using DocStringExtensions
+
+import KernelDensityEstimatePlotting: plot, drawHorDens, plotKDE
+import IncrementalInference: CliqGibbsMC, DebugCliqMCMC
+import Graphs: plot
+import Gadfly: plot
+
+export
+  # Associated with IncrementalInference
+  investigateMultidimKDE,
+  drawHorDens,
+  drawHorBeliefsList,
+  spyCliqMat,
+  plotKDE,
+  plotKDEofnc,
+  plotKDEresiduals,
+  plotMCMC,
+  plotKDE,
+  plotUpMsgsAtCliq,
+  plotPriorsAtCliq,
+  investigateMultidimKDE,
+  draw,
+  plot,
+  whosWith,
+  drawUpMsgAtCliq,
+  dwnMsgsAtCliq,
+  drawPose2DMC!,
+  mcmcPose2D!,
+  # drawUpMCMCPose2D!,
+  # drawDwnMCMCPose2D!,
+  drawLbl,
+  predCurrFactorBeliefs,
+  drawFactorBeliefs,
+  localProduct,
+  plotLocalProduct,
+  saveplot,
+  animateVertexBelief,
+  getColorsByLength,
+
+  # Associated with RoME
+  togglePrtStbLines,
+  plotLsrScanFeats,
+  drawFeatTrackers,
+  saveImgSeq,
+  stbPrtLineLayers!,
+  drawPoses,
+  drawLandms,
+  drawPosesLandms,
+  drawSubmaps,
+  investigatePoseKDE,
+  drawMarginalContour,
+  accumulateMarginalContours,
+  plotPose3Pairs,
+  progressExamplePlot,
+  plotTrckStep
+
+
+include("SolverVisualization.jl")
+
+include("RobotViz.jl")
+
+
+
+end
